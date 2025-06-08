15417bd1b3f15a1c283dcfb71ebb7a55df8fb9be
Ezio Borges <eziopborges@gmail.com>
2025-06-08 18:53:21 -0300

    refactor: improve layout and styling of Login form

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -9,32 +9,35 @@ const Login = () => {
           className="w-25 rounded shadow p-5 d-flex flex-column justify-content-center"
           data-testid="login-form-area"
         >
-          <div className="d-flex flex-column align-items-start">
+          <div className="row mb-2">
+            <h1>Entre no VALE</h1>
+          </div>
+          <div className="row d-flex flex-column align-items-start m-3">
             <label
               htmlFor="login-input-email"
               data-testid="login-label-email"
-              className="fs-5 fw-bold"
+              className="fs-5 fw-bold text-start"
             >
               Email:
             </label>
             <input
-              className="w-100 p-2 mt-2"
+              className="p-2 mt-2"
               type="email"
               id="login-input-email"
               placeholder="Digite seu email"
               data-testid="login-input-email"
             />
           </div>
-          <div className="d-flex flex-column align-items-start mt-3">
+          <div className="row d-flex flex-column align-items-start m-3">
             <label
-              className="fs-5 fw-bold"
+              className="fs-5 fw-bold text-start"
               htmlFor="login-input-password"
               data-testid="login-label-password"
             >
               Senha:
             </label>
             <input
-              className="w-100 p-2 mt-2"
+              className="p-2 mt-2"
               type="text"
               data-testid="login-input-password"
               placeholder="Digite sua senha"
